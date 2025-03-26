54021c924a48b3b01e4b3f9ca378a8ba2e4b65f7
moidardu <moidardu@gmail.com>
2025-03-26 17:57:29 +0200

    finally managed to solve angle case motor locating. this required to compare a float. which is something CPP doesnt like, needed to convert to int. log 110

--- libraries/SITL/SIM_Motor.cpp
+++ libraries/SITL/SIM_Motor.cpp
@@ -181,13 +181,56 @@ void Motor::setup_params(uint16_t _pwm_min, uint16_t _pwm_max, float _spin_min, 
     true_prop_area = _true_prop_area;
     momentum_drag_coefficient = _momentum_drag_coefficient;
     diagonal_size = _diagonal_size;
-    
+
     if (!_position.is_zero()) {
         position = _position;
     } else {
-        position.x = cosf(radians(angle)) * _diagonal_size;
-        position.y =  sinf(radians(angle)) * _diagonal_size;
+        
+        if (int(angle) == 22) //motor 1
+        {
+            position.x = cosf(radians(angle)) * _diagonal_size;
+            position.y =  sinf(radians(angle)) * _diagonal_size;
+        }
+ 
+ 
+        else if (int(angle) == -158) //motor 2
+        {
+            position.x = cosf(radians(angle)) * _diagonal_size;
+            position.y =  sinf(radians(angle)) * _diagonal_size;
+        }
+        else if (int(angle) == 43) //motor 3
+        {
+            position.x = cosf(radians(angle)) * _diagonal_size;
+            position.y =  sinf(radians(angle)) * _diagonal_size;
+        }
+        else if (int(angle) == 158) //motor 4
+        {
+            position.x = cosf(radians(angle)) * _diagonal_size;
+            position.y =  sinf(radians(angle)) * _diagonal_size;
+        }
+        else if (int(angle) == -22) //motor 5
+        {
+            position.x = cosf(radians(angle)) * _diagonal_size;
+            position.y =  sinf(radians(angle)) * _diagonal_size;
+        }
+        else if (int(angle) == -137) //motor 6
+        {
+            position.x = cosf(radians(angle)) * _diagonal_size;
+            position.y =  sinf(radians(angle)) * _diagonal_size;
+        }
+        else if (int(angle) == -43) //motor 7
+        {
+            position.x = cosf(radians(angle)) * _diagonal_size;
+            position.y =  sinf(radians(angle)) * _diagonal_size;
+        }
+        else if(int(angle) == 137) //motor 8
+        {
+            position.x = cosf(radians(angle)) * _diagonal_size;
+            position.y =  sinf(radians(angle)) * _diagonal_size;
+        }
         position.z = 0;
+ 
+        
     }
 
     if (!_thrust_vector.is_zero()) {
