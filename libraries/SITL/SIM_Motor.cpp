124fcc72038a42c4ddc5e7d8fb77f19e0eda5487
moidardu <moidardu@gmail.com>
2025-03-26 17:09:14 +0200

    else if

--- libraries/SITL/SIM_Motor.cpp
+++ libraries/SITL/SIM_Motor.cpp
@@ -189,7 +189,8 @@ void Motor::setup_params(uint16_t _pwm_min, uint16_t _pwm_max, float _spin_min, 
         // position.y =  sinf(radians(angle)) * _diagonal_size;
         position.z = 0;
 
-        if (angle ==22){ //motor 1
+        if (angle == 22) //motor 1
+        { 
             position.x = cosf(radians(angle)) * _diagonal_size;
             position.y =  sinf(radians(angle)) * _diagonal_size;
         }
@@ -224,7 +225,7 @@ void Motor::setup_params(uint16_t _pwm_min, uint16_t _pwm_max, float _spin_min, 
             position.x = cosf(radians(angle)) * _diagonal_size;
             position.y =  sinf(radians(angle)) * _diagonal_size;
         }
-        else (angle == 137) //motor 8
+        else if (angle == 137) //motor 8
         {
             position.x = cosf(radians(angle)) * _diagonal_size;
             position.y =  sinf(radians(angle)) * _diagonal_size;
