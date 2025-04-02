7118977c2c1d1f9b65708ef7c2c78eee2b4f4f41
moidardu <moidardu@gmail.com>
2025-04-02 13:50:54 +0300

    test - back to h normal m1 loss (redo log 134) -- yup works

--- libraries/SITL/SIM_Frame.cpp
+++ libraries/SITL/SIM_Frame.cpp
@@ -201,6 +201,7 @@ static Motor octa_motors[] =
 */
 /* this is the CLASSIC OCTA H - change the yaw factor in SIM to match emperical results. front right boom goes down
 
+*/
 
 static Motor octa_motors[] =
 {
@@ -213,7 +214,6 @@ static Motor octa_motors[] =
     Motor(AP_MOTORS_MOT_7, 7,  AP_MOTORS_MATRIX_YAW_FACTOR_CW,  7),
     Motor(AP_MOTORS_MOT_8, 8,  AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3)
 };
-*/
 
 
 
@@ -234,7 +234,6 @@ static Motor octa_motors[] =
 */
 
 /*this is the WING OCTA H 
-*/
 static Motor octa_motors[] =
 {
     Motor(AP_MOTORS_MOT_1, 1,  AP_MOTORS_MATRIX_YAW_FACTOR_CCW,  1),
@@ -246,6 +245,7 @@ static Motor octa_motors[] =
     Motor(AP_MOTORS_MOT_7, 7,  AP_MOTORS_MATRIX_YAW_FACTOR_CW,  7),
     Motor(AP_MOTORS_MOT_8, 8,  AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3)
 };
+*/
 
 
 static Motor octa_dji_x_motors[] =
