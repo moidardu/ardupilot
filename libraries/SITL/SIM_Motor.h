7b70db13bf149a31a77fd7abf4dce15d661c9552
moidardu <moidardu@gmail.com>
2025-04-02 17:31:36 +0300

    log 163 normal H m1 loss no angle - full scale modulus

--- libraries/SITL/SIM_Motor.h
+++ libraries/SITL/SIM_Motor.h
@@ -50,7 +50,7 @@ public:
     // float twist_angle_factor_sin = 0.02; //twist factor for 1.1 degrees, PRE convergence - reverse H no motor angle, M1 loss
 
     // reveresed H octa values:
-    float twist_angle_factor_sin = 0.0; //twist factor for iterations
+    float twist_angle_factor_sin = 0.019; //twist factor for iterations
 
 
 
