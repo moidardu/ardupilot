d5e21fba7cf1ecded68a9999dda4a6031c1d1438
moidardu <moidardu@gmail.com>
2025-04-03 16:26:44 +0300

    log 189 inner motor yaw impacted with notifiying the motor matrix

--- libraries/AP_Motors/AP_MotorsMatrix.cpp
+++ libraries/AP_Motors/AP_MotorsMatrix.cpp
@@ -968,12 +968,12 @@ bool AP_MotorsMatrix::setup_octa_matrix(motor_frame_type frame_type)
         static const AP_MotorsMatrix::MotorDefRaw motors[] {
             { -0.4f,    1.0f, AP_MOTORS_MATRIX_YAW_FACTOR_CCW,  1 },
             {  0.4f,   -1.0f, AP_MOTORS_MATRIX_YAW_FACTOR_CCW,  5 },
-            { -0.4f,   0.43f, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 2 },
+            { -0.4f,   0.43f, AP_MOTORS_MATRIX_YAW_FACTOR_CCW+0.3, 2 },
             { -0.4f,   -1.0f, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 4 },
             {  0.4f,    1.0f, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 8 },
-            {  0.4f,  -0.43f, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 6 },
-            {  0.4f,   0.43f, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  7 },
-            { -0.4f,  -0.43f, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3 },
+            {  0.4f,  -0.43f, AP_MOTORS_MATRIX_YAW_FACTOR_CCW+0.3, 6 },
+            {  0.4f,   0.43f, AP_MOTORS_MATRIX_YAW_FACTOR_CW-0.3,  7 },
+            { -0.4f,  -0.43f, AP_MOTORS_MATRIX_YAW_FACTOR_CW-0.3,  3 },
         };
         add_motors_raw(motors, ARRAY_SIZE(motors));
         break;
