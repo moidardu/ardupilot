b679ae53c4bcd8b71e57d65cd8d605a32d199952
moidardu <moidardu@gmail.com>
2025-04-20 10:05:20 +0300

    log 218 - limit of stability with constant angle change - all motors with 1deg angle adverse to yaw stability

--- libraries/SITL/SIM_Motor.h
+++ libraries/SITL/SIM_Motor.h
@@ -58,7 +58,7 @@ public:
 
     // float motor_angle_factor = 0.00; // ~ 0 degree motor angled in/out
     // 
-    float motor_angle_factor = 0.02; // motor angle factor for uniform yaw bias, Y.Out is from exp. is 0.14, this correlates to 0.7 degrees
+    float motor_angle_factor = 0.0183; // motor angle factor for uniform yaw bias, Y.Out is from exp. is 0.14, this correlates to 0.7 degrees
     float motor_angle_factor_motor_loss_variance = 2.5*motor_angle_factor; //i.e. motor 6 has 0.8 deg, but motor 4 has 0.6
     // float motor_angle_multiplier = 1/0.43; //factor of ~2.3, i.e. 7 degrees
     float motor_angle_multiplier = 0;
