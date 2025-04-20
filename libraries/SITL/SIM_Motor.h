a1d1e9de1629c859fb8ac5ec2271ad8d526b8bc2
moidardu <moidardu@gmail.com>
2025-04-20 10:31:35 +0300

    log 223 - removed 6% COG  imbalance. now able to keep yaw balance, even with 1 deg motor angle in direction of adverse yaw for each of the motors (exept for m6, which is turned off).

--- libraries/SITL/SIM_Motor.h
+++ libraries/SITL/SIM_Motor.h
@@ -50,10 +50,10 @@ public:
     // float twist_angle_factor_sin = 0.02; //twist factor for 1.1 degrees, PRE convergence - reverse H no motor angle, M1 loss
 
     // reveresed H octa values:
-    // float twist_angle_factor_sin = 0.012; //twist factor in frame
-    float twist_angle_factor_sin = 0.00; //twist factor in frame
+    float twist_angle_factor_sin = 0.012; //twist factor in frame
+    // float twist_angle_factor_sin = 0.00; //twist factor in frame
 
-    float p_out_compensation_dist = 0.06; 
+    float p_out_compensation_dist = 0.0; 
 
 
     // float motor_angle_factor = 0.00; // ~ 0 degree motor angled in/out
